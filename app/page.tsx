57c60f0808be677f31d1932db6ec8403c8876add
Harry Dhillon <hdhillon478@gmail.com>
2025-10-25 08:05:53 +0100

    feat: update Home component to display title with new styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,8 @@
 export default function Home() {
-  return <div>Home Page</div>;
+  return (
+    <div>
+      <h1 className='h1-bold'>Just Stack More</h1>
+      <h1 className='h1-bold font-space-grotesk'>Just Stack More</h1>
+    </div>
+  );
 }
