491a1428b5e7426e67af92035550fa6d887c05dd
Harry Dhillon <hdhillon478@gmail.com>
2025-10-25 15:02:47 +0100

    feat: implement Navbar component with logo and placeholder text

--- components/navigation/navbar/index.tsx
+++ components/navigation/navbar/index.tsx
@@ -1,3 +1,25 @@
+import Image from "next/image";
+import Link from "next/link";
+
 export default function Navbar() {
-  return <nav className='flex-between'>Navbar</nav>;
+  return (
+    <nav className='flex-between background-light900_dark200 fixed z-50 w-full p-6 dark:shadow-none sm:px-12 shadow-light-300 gap-5'>
+      <Link href='/' className='flex items-center gap-1'>
+        <Image
+          src='/images/site-logo.svg'
+          alt='Just Stack More Logo'
+          width={23}
+          height={23}
+        />
+
+        <p className='h2-bold font-space-grotesk text-dark-100 dark:text-light-900 max-sm:hidden'>
+          Just <span className='text-primary-500'>Stack</span> More
+        </p>
+      </Link>
+
+      <p>Global Search</p>
+
+      <div className='flex-between gap-5'>Theme</div>
+    </nav>
+  );
 }
